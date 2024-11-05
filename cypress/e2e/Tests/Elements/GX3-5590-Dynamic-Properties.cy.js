0ce40e3c86655ca8ac826aeef414a85cf164b2c3
Diego González Fernández <verogeid@gmail.com>
2024-11-05 09:59:40 +0100

    refactor (GX3-5590) Comprobación de existencia de elementos pasa a la PRC

--- cypress/e2e/Tests/Elements/GX3-5590-Dynamic-Properties.cy.js
+++ cypress/e2e/Tests/Elements/GX3-5590-Dynamic-Properties.cy.js
@@ -8,23 +8,21 @@ describe('US GX3-5590 | ToolsQA | Elements | Dynamic Properties', () => {
 	beforeEach('PRC: Abrir la url dynamic-properties de ToolsQA', () => {
 		cy.visit('https://demoqa.com/dynamic-properties');
 		cy.url().should('contain', 'dynamic-properties');
-	});
 
-	it('US # GX3-5590 | TC#01: Validar la existencia de los elementos', () => {
 		cy.get('[class="col-12 mt-4 col-md-6"] p').should('exist');
 		cy.get('[class="col-12 mt-4 col-md-6"] button.mt-4.btn.btn-primary').should('exist').and('have.length', 2); // visibleButton se crea y hace visible 5 segundos después
 	});
 
-	it('US # GX3-5590 | TC#02: Validar el texto del tag paragraph', () => {
+	it('US # GX3-5590 | TC#01: Validar el texto del tag paragraph', () => {
 		dynamicPropertiesPage.get.txtElement().should('contain.text', 'This text has random Id').and('be.visible');
 	});
 
-	it('US # GX3-5590 | TC#03: Validar el cambio de estado del button "enableAfter"', () => {
+	it('US # GX3-5590 | TC#02: Validar el cambio de estado del button "enableAfter"', () => {
 		cy.get('[class="col-12 mt-4 col-md-6"] button#enableAfter.mt-4.btn.btn-primary').should('be.not.enabled');
 		dynamicPropertiesPage.get.btnEnable().should('be.enabled');
 	});
 
-	it('US # GX3-5590 | TC#04: Validar el cambio de color del button "colorChange"', () => {
+	it('US # GX3-5590 | TC#03: Validar el cambio de color del button "colorChange"', () => {
 		cy.get('[class="col-12 mt-4 col-md-6"] button#colorChange.mt-4.btn.btn-primary').should('be.visible');
 		dynamicPropertiesPage.get.btnColor().should('have.class', 'text-danger');
 	});
