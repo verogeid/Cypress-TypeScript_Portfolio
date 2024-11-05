302c1c7f6aeb434d9963bf5cb832ca646bb0e72d
Diego González Fernández <verogeid@gmail.com>
2024-11-05 11:57:38 +0100

    fix (GX3-5590) Se incorpora a la clase 'dynamicPropertiesPage' la localización de los elementos al cargar la página, para una mejor comprensión del código

--- cypress/e2e/Tests/Elements/GX3-5590-Dynamic-Properties.cy.js
+++ cypress/e2e/Tests/Elements/GX3-5590-Dynamic-Properties.cy.js
@@ -9,7 +9,7 @@ describe('US GX3-5590 | ToolsQA | Elements | Dynamic Properties', () => {
 		cy.visit('https://demoqa.com/dynamic-properties');
 		cy.url().should('contain', 'dynamic-properties');
 
-		cy.get('[class="col-12 mt-4 col-md-6"] p').should('exist');
+		dynamicPropertiesPage.get.txtElement().should('exist');
 		cy.get('[class="col-12 mt-4 col-md-6"] button.mt-4.btn.btn-primary').should('exist').and('have.length', 2); // visibleButton se crea y hace visible 5 segundos después
 	});
 
@@ -18,17 +18,17 @@ describe('US GX3-5590 | ToolsQA | Elements | Dynamic Properties', () => {
 	});
 
 	it('US # GX3-5590 | TC#02: Validar el cambio de estado del button "enableAfter"', () => {
-		cy.get('[class="col-12 mt-4 col-md-6"] button#enableAfter.mt-4.btn.btn-primary').should('be.not.enabled');
-		dynamicPropertiesPage.get.btnEnable().should('be.enabled');
+		dynamicPropertiesPage.get.firstLoad.btnEnable().should('be.not.enabled');
+		dynamicPropertiesPage.get.lastLoad.btnEnable().should('be.enabled');
 	});
 
 	it('US # GX3-5590 | TC#03: Validar el cambio de color del button "colorChange"', () => {
-		cy.get('[class="col-12 mt-4 col-md-6"] button#colorChange.mt-4.btn.btn-primary').should('be.visible');
-		dynamicPropertiesPage.get.btnColor().should('have.class', 'text-danger');
+		dynamicPropertiesPage.get.firstLoad.btnColor().should('be.visible');
+		dynamicPropertiesPage.get.lastLoad.btnColor().should('have.class', 'text-danger');
 	});
 
 	it('US # GX3-5590 | TC#04: Validar el cambio de visibilidad del button "visibleAfter"', () => {
-		cy.get('[class="col-12 mt-4 col-md-6"] button#visibleAfter.mt-4.btn.btn-primary').should('not.exist');
-		dynamicPropertiesPage.get.btnVisible().should('exist').and('be.visible');
+		dynamicPropertiesPage.get.firstLoad.btnVisible().should('not.exist');
+		dynamicPropertiesPage.get.lastLoad.btnVisible().should('exist').and('be.visible');
 	});
 });
--- cypress/support/pages/GX3-5590-Dynamic-Properties.Page.js
+++ cypress/support/pages/GX3-5590-Dynamic-Properties.Page.js
@@ -1,9 +1,16 @@
 class DynamicProperties {
 	get = {
 		txtElement: () => cy.get('[class="col-12 mt-4 col-md-6"] p'),
-		btnEnable: () => cy.get('[class="col-12 mt-4 col-md-6"] button#enableAfter', { timeout: 5000 }),
-		btnColor: () => cy.get('[class="col-12 mt-4 col-md-6"] button#colorChange', { timeout: 5000 }),
-		btnVisible: () => cy.get('[class="col-12 mt-4 col-md-6"] button#visibleAfter', { timeout: 5000 })
+		firstLoad: {
+			btnEnable: () => cy.get('[class="col-12 mt-4 col-md-6"] button#enableAfter.mt-4.btn.btn-primary'),
+			btnColor: () => cy.get('[class="col-12 mt-4 col-md-6"] button#colorChange.mt-4.btn.btn-primary'),
+			btnVisible: () => cy.get('[class="col-12 mt-4 col-md-6"] button#visibleAfter.mt-4.btn.btn-primary')
+		},
+		lastLoad: {
+			btnEnable: () => cy.get('[class="col-12 mt-4 col-md-6"] button#enableAfter', { timeout: 5000 }),
+			btnColor: () => cy.get('[class="col-12 mt-4 col-md-6"] button#colorChange', { timeout: 5000 }),
+			btnVisible: () => cy.get('[class="col-12 mt-4 col-md-6"] button#visibleAfter', { timeout: 5000 })
+		}
 	};
 }
 
