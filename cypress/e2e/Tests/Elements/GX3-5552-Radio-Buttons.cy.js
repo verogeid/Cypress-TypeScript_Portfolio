73b8df86e20762644e7a1765fc21ccf6c5ddddb6
Diego González Fernández <verogeid@gmail.com>
2024-11-06 09:53:10 +0100

    fix (GX3-5552) Eliminados casos de prueba innecesarios, integrandolos en los validos. Eliminación de ramas pendientes de PR Approved

--- cypress/e2e/Tests/Elements/GX3-5552-Radio-Buttons.cy.js
+++ cypress/e2e/Tests/Elements/GX3-5552-Radio-Buttons.cy.js
@@ -1,40 +1,32 @@
 describe('US GX3-5552 | ToolsQA | Elements | Radio Buttons', () => {
-	cy.on('uncaught:exception', (err, runnable) => {
-		return false;
-	});
-
 	beforeEach('PRC: Abrir la url radio buttons de ToolsQA', () => {
 		cy.visit('https://demoqa.com/radio-button');
 		cy.url().should('contain', 'radio-button');
 	});
 
-	it('US # GX3-5552 | TC#01: Validar existencia de los radio', () => {
-		cy.get('input[type=radio]').should('exist').and('have.length', 3);
-
-		cy.contains('Yes').should('be.visible');
-		cy.contains('Impressive').should('be.visible');
-		cy.contains('No').should('be.visible');
-	});
-
-	it('US # GX3-5552 | TC#02 Validar que ningún botón este seleccionado por DEFECTO', () => {
+	it('US # GX3-5552 | TC#01: Validar selección del radio button “Yes” y visualizar mensaje', () => {
+		cy.get('input[type=radio]').should('exist');
 		cy.get('#yesRadio.custom-control-input').should('not.be.checked');
-		cy.get('#impressiveRadio.custom-control-input').should('not.be.checked');
-		cy.get('#noRadio.custom-control-input').should('not.be.checked');
-	});
-
-	it('US # GX3-5552 | TC#03: Validar selección del radio button “Yes” y visualizar mensaje', () => {
 		cy.get('#yesRadio').should('be.enabled');
 		cy.get('label[for="yesRadio"]').click();
+
 		cy.get('.text-success').should('have.text', 'Yes').and('be.visible');
 	});
 
-	it('US # GX3-5552 | TC#04: Validar selección del radio button “Impressive” y visualizar mensaje', () => {
+	it('US # GX3-5552 | TC#02: Validar selección del radio button “Impressive” y visualizar mensaje', () => {
+		cy.get('input[type=radio]').should('exist');
+		cy.contains('Impressive').should('be.visible');
+		cy.get('#impressiveRadio.custom-control-input').should('not.be.checked');
 		cy.get('#impressiveRadio').should('be.enabled');
 		cy.get('label[for="impressiveRadio"]').click();
+
 		cy.get('.text-success').should('have.text', 'Impressive').and('be.visible');
 	});
 
-	it('US # GX3-5552 | TC#05: Validar que NO se puede seleccionar el radio button “No”', () => {
+	it('US # GX3-5552 | TC#03: Validar que NO se puede seleccionar el radio button “No”', () => {
+		cy.get('input[type=radio]').should('exist');
+		cy.contains('No').should('be.visible');
+		cy.get('#noRadio.custom-control-input').should('not.be.checked');
 		cy.get('#noRadio').should('be.disabled');
 	});
 });
