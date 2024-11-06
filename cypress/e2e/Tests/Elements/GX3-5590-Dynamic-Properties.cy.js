6c10bde3eb1bcdd93653dc1099ab656db024c5fb
Diego González Fernández <verogeid@gmail.com>
2024-11-06 09:34:46 +0100

    fix (GX3-5590) Corregido codigo según sugerencias. Eliminadas las ramas pendientes de PR approved que se pusheaban por error a la vez.

--- cypress/e2e/Tests/Elements/GX3-5590-Dynamic-Properties.cy.js
+++ cypress/e2e/Tests/Elements/GX3-5590-Dynamic-Properties.cy.js
@@ -1,34 +1,25 @@
-import { dynamicPropertiesPage } from '@pages/GX3-5590-Dynamic-Properties.Page';
-
 describe('US GX3-5590 | ToolsQA | Elements | Dynamic Properties', () => {
-	cy.on('uncaught:exception', (err, runnable) => {
-		return false;
-	});
-
 	beforeEach('PRC: Abrir la url dynamic-properties de ToolsQA', () => {
 		cy.visit('https://demoqa.com/dynamic-properties');
 		cy.url().should('contain', 'dynamic-properties');
-
-		dynamicPropertiesPage.get.txtElement().should('exist');
-		cy.get('[class="col-12 mt-4 col-md-6"] button.mt-4.btn.btn-primary').should('exist').and('have.length', 2); // visibleButton se crea y hace visible 5 segundos después
 	});
 
 	it('US # GX3-5590 | TC#01: Validar el texto del tag paragraph', () => {
-		dynamicPropertiesPage.get.txtElement().should('contain.text', 'This text has random Id').and('be.visible');
+		cy.get('[class="col-12 mt-4 col-md-6"] p').should('exist').and('contain.text', 'This text has random Id').and('be.visible');
 	});
 
 	it('US # GX3-5590 | TC#02: Validar el cambio de estado del button "enableAfter"', () => {
-		dynamicPropertiesPage.get.firstLoad.btnEnable().should('be.not.enabled');
-		dynamicPropertiesPage.get.lastLoad.btnEnable().should('be.enabled');
+		cy.get('[class="col-12 mt-4 col-md-6"] button#enableAfter.mt-4.btn.btn-primary').should('exist').and('be.not.enabled');
+		cy.get('[class="col-12 mt-4 col-md-6"] button#enableAfter', { timeout: 5000 }).should('exist').and('be.enabled');
 	});
 
 	it('US # GX3-5590 | TC#03: Validar el cambio de color del button "colorChange"', () => {
-		dynamicPropertiesPage.get.firstLoad.btnColor().should('be.visible');
-		dynamicPropertiesPage.get.lastLoad.btnColor().should('have.class', 'text-danger');
+		cy.get('[class="col-12 mt-4 col-md-6"] button#colorChange.mt-4.btn.btn-primary').should('exist').and('be.visible');
+		cy.get('[class="col-12 mt-4 col-md-6"] button#colorChange', { timeout: 5000 }).should('have.class', 'text-danger');
 	});
 
 	it('US # GX3-5590 | TC#04: Validar el cambio de visibilidad del button "visibleAfter"', () => {
-		dynamicPropertiesPage.get.firstLoad.btnVisible().should('not.exist');
-		dynamicPropertiesPage.get.lastLoad.btnVisible().should('exist').and('be.visible');
+		cy.get('[class="col-12 mt-4 col-md-6"] button#visibleAfter.mt-4.btn.btn-primary').should('not.exist');
+		cy.get('[class="col-12 mt-4 col-md-6"] button#visibleAfter', { timeout: 5000 }).should('exist').and('be.visible');
 	});
 });
