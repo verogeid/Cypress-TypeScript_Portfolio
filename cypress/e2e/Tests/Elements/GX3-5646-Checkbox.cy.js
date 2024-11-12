361bc048b7587b4dd3156db80c7f1f0aefe96123
Diego González Fernández <verogeid@gmail.com>
2024-11-12 12:38:03 +0100

    test (GX3-5646) Implementando validaciones para toggle y checkbox

--- cypress/e2e/Tests/Elements/GX3-5646-Checkbox.cy.js
+++ cypress/e2e/Tests/Elements/GX3-5646-Checkbox.cy.js
@@ -8,24 +8,24 @@ describe('US # GX3-5646 | ToolsQA | Elements | Checkbox', () => {
 		// Localizar el botón Expand
 		cy.get('button.rct-option.rct-option-expand-all').as('btnExpandAll');
 
-		// Localizar el botón de colapso de las ramas
-		cy.get('button.rct-collapse.rct-collapse-btn').as('btnCollapse');
-		// Localizar la primera instancia de collapse de la rama (la posición 0 contiene All)
-		cy.get('svg[class^="rct-icon rct-icon-expand-"]').eq(1).as('imgCollapse');
+		// Localizar el botón de toggle de las ramas
+		cy.get('button.rct-collapse.rct-collapse-btn').as('btnToggle');
+		// Localizar la primera instancia de toggle de la rama (la posición 0 contiene All)
+		cy.get('svg[class^="rct-icon rct-icon-expand-"]').eq(1).as('imgToggle');
 		// Localizar los checkbox ['check' | 'uncheck' | 'half-check']
 		cy.get('svg[class^="rct-icon rct-icon-"][class$="check"]').as('chkNode');
 
 		// Debe haber al menos 1
-		cy.get('@btnCollapse').should('exist').and('have.length.above', 0);
-		cy.get('@imgCollapse').should('exist'); // debe existir, pero no sabemos si esta abierta o no
+		cy.get('@btnToggle').should('exist').and('have.length.above', 0);
+		cy.get('@imgToggle').should('exist'); // debe existir, pero no sabemos si esta abierta o no
 		cy.get('@chkNode').should('exist').and('have.length.above', 0);
 
 		// tras hacer click en Expand
 		cy.get('@btnExpandAll').should('exist').click();
 
-		// Localizar los iconos de colapso de la rama
-		cy.get('svg[class="rct-icon rct-icon-expand-open"]').as('imgCollapseOpen');
-		cy.get('@imgCollapseOpen').should('exist'); //debe existir y estar abierta tras el click
+		// Localizar los iconos de toggle de la rama
+		cy.get('svg[class="rct-icon rct-icon-expand-open"]').as('imgToggleOpen');
+		cy.get('@imgToggleOpen').should('exist'); //debe existir y estar abierta tras el click
 
 		// Localizar los icono de la ramas cerrados. No debiera haber ninguno
 		cy.get('svg[class="rct-icon rct-icon-expand-close"]').should('not.exist');
@@ -42,24 +42,47 @@ describe('US # GX3-5646 | ToolsQA | Elements | Checkbox', () => {
 		cy.get('button.rct-option.rct-option-collapse-all').as('btnCollapseAll');
 
 		// Localizar el botón de colapso de las ramas
-		cy.get('.rct-collapse.rct-collapse-btn').as('btnCollapse');
+		cy.get('.rct-collapse.rct-collapse-btn').as('btnToggle');
 		// Localizar el primer icono de colapso de las ramas [open | close]
-		cy.get('svg[class^="rct-icon rct-icon-expand-"]').eq(1).as('imgCollapse');
+		cy.get('svg[class^="rct-icon rct-icon-expand-"]').eq(1).as('imgToggle');
 		// Localizar los checkbox ['check' | 'uncheck' | 'half-check']
 		cy.get('svg[class^="rct-icon rct-icon-"][class$="check"]').as('chkNode');
 
 		// Debe haber al menos 1
-		cy.get('@btnCollapse').should('exist').and('have.length.above', 0);
+		cy.get('@btnToggle').should('exist').and('have.length.above', 0);
 		cy.get('@chkNode').should('exist').and('have.length.above', 0);
-		cy.get('@imgCollapse').should('exist').and('have.length.above', 0);
+		cy.get('@imgToggle').should('exist').and('have.length.above', 0);
 
 		// tras hacer click en Collapse
 		cy.get('@btnCollapseAll').should('exist').click();
-		cy.get('svg[class="rct-icon rct-icon-expand-close"]').as('imgCollapseOpen');
+		cy.get('svg[class="rct-icon rct-icon-expand-close"]').as('imgToggleOpen');
 
 		// debe haber sólo 1
 		cy.get('@chkNode').should('have.length', 1);
-		cy.get('@btnCollapse').should('exist').and('have.length', 1);
-		cy.get('@imgCollapseOpen').should('exist'); // y estar cerrada la rama
+		cy.get('@btnToggle').should('exist').and('have.length', 1);
+		cy.get('@imgToggleOpen').should('exist'); // y estar cerrada la rama
 	});
+
+	it('GX3-5661 | TC#03: Validar comportamiento de toggles intermedios', () => {
+		//Abrir la rama principal
+		cy.get('.rct-collapse.rct-collapse-btn').first().as('btnToggle').click();
+
+		// Obtener la lista de nodos
+		cy.get('li[class^="rct-node"]').as('lstNodes');
+
+		var intListLength = 0;
+
+		cy.get('@lstNodes')
+			.should('have.length.above', 0)
+			.then(elm => {
+				intListLength = elm.length;
+			});
+		window.alert(intListLength);
+
+		const indexRandom = Cypress._.random(0, intListLength - 1);
+
+		cy.get('@lstNodes').eq(indexRandom).click();
+	});
+
+	it('GX3-5661 | TC#04: Validar que muestre mensaje al marcar checkboxs', () => {});
 });
