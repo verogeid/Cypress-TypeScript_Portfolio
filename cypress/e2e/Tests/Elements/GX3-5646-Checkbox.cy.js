709ebd687a7108d65f695e0441b4bed68fd415fb
Diego González Fernández <verogeid@gmail.com>
2024-11-12 11:03:18 +0100

    test (GX3-5646) Pruebas para botón Expand y Collapse completadasD

--- cypress/e2e/Tests/Elements/GX3-5646-Checkbox.cy.js
+++ cypress/e2e/Tests/Elements/GX3-5646-Checkbox.cy.js
@@ -10,46 +10,56 @@ describe('US # GX3-5646 | ToolsQA | Elements | Checkbox', () => {
 
 		// Localizar el botón de colapso de las ramas
 		cy.get('button.rct-collapse.rct-collapse-btn').as('btnCollapse');
-		// Localizar el icono de colapso de las ramas [open | close]
-		//cy.get('svg[clash^="rct-icon rct-icon-expand-"]').as('imgCollapse');
+		// Localizar la primera instancia de collapse de la rama (la posición 0 contiene All)
+		cy.get('svg[class^="rct-icon rct-icon-expand-"]').eq(1).as('imgCollapse');
 		// Localizar los checkbox ['check' | 'uncheck' | 'half-check']
 		cy.get('svg[class^="rct-icon rct-icon-"][class$="check"]').as('chkNode');
 
 		// Debe haber al menos 1
 		cy.get('@btnCollapse').should('exist').and('have.length.above', 0);
-		//cy.get('@imgCollapse').should('exist').and('have.length.above', 0);
+		cy.get('@imgCollapse').should('exist'); // debe existir, pero no sabemos si esta abierta o no
 		cy.get('@chkNode').should('exist').and('have.length.above', 0);
 
 		// tras hacer click en Expand
 		cy.get('@btnExpandAll').should('exist').click();
 
+		// Localizar los iconos de colapso de la rama
+		cy.get('svg[class="rct-icon rct-icon-expand-open"]').as('imgCollapseOpen');
+		cy.get('@imgCollapseOpen').should('exist'); //debe existir y estar abierta tras el click
+
+		// Localizar los icono de la ramas cerrados. No debiera haber ninguno
+		cy.get('svg[class="rct-icon rct-icon-expand-close"]').should('not.exist');
+
 		// debe haber más de 1 (en principio son 17, pero puede cambiar)
 		cy.get('@chkNode').should('have.length.above', 1);
-		//cy.get('@imgCollapse').should('have.class', 'rct-icon rct-icon-expand-close');
 	});
 
 	it('GX3-5661 | TC#02: Validar que el boton "Collapse All" cierre todas las ramas', () => {
+		// PRC: Localizar el botón Expand y hacer click
+		cy.get('button.rct-option.rct-option-expand-all').as('btnExpandAll').click();
+
 		// Localizar el botón Collapse All
 		cy.get('button.rct-option.rct-option-collapse-all').as('btnCollapseAll');
 
 		// Localizar el botón de colapso de las ramas
 		cy.get('.rct-collapse.rct-collapse-btn').as('btnCollapse');
-		// Localizar el icono de colapso de las ramas [open | close]
-		//cy.get('svg[clash^="rct-icon rct-icon-expand-"]').as('imgCollapse');
+		// Localizar el primer icono de colapso de las ramas [open | close]
+		cy.get('svg[class^="rct-icon rct-icon-expand-"]').eq(1).as('imgCollapse');
 		// Localizar los checkbox ['check' | 'uncheck' | 'half-check']
 		cy.get('svg[class^="rct-icon rct-icon-"][class$="check"]').as('chkNode');
 
 		// Debe haber al menos 1
 		cy.get('@btnCollapse').should('exist').and('have.length.above', 0);
 		cy.get('@chkNode').should('exist').and('have.length.above', 0);
-		//cy.get('@imgCollapse').should('exist').and('have.length.above', 0);
+		cy.get('@imgCollapse').should('exist').and('have.length.above', 0);
 
 		// tras hacer click en Collapse
 		cy.get('@btnCollapseAll').should('exist').click();
+		cy.get('svg[class="rct-icon rct-icon-expand-close"]').as('imgCollapseOpen');
 
 		// debe haber sólo 1
 		cy.get('@chkNode').should('have.length', 1);
 		cy.get('@btnCollapse').should('exist').and('have.length', 1);
-		//cy.get('@imgCollapse').should('exist').and('have.length', 1).and('have.class', 'rct-icon rct-icon-expand-open');
+		cy.get('@imgCollapseOpen').should('exist'); // y estar cerrada la rama
 	});
 });
