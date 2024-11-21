3daab71d343b413d62119f6aa8341c64b1e6bded
Diego González Fernández <verogeid@gmail.com>
2024-11-21 11:19:01 +0100

    fix (GX3-5646) Improved code visibility and added checks on toggle behavior

--- cypress/e2e/Tests/Elements/GX3-5646-Checkbox.cy.js
+++ cypress/e2e/Tests/Elements/GX3-5646-Checkbox.cy.js
@@ -7,8 +7,12 @@ describe('US # GX3-5646 | ToolsQA | Elements | Checkbox', () => {
 	});
 
 	it('GX3-5661 | TC#01: Validar que el boton "Expand All" abra todas las ramas', () => {
+		const BUTTON_EXPAND_ALL = checkboxTreePage.getSelector.buttonExpandAll();
+		const IMG_TOGGLE_OPENED = checkboxTreePage.getSelector.imgToggleOpened();
+		const IMG_TOGGLE_CLOSED = checkboxTreePage.getSelector.imgToggleClosed();
+
 		// Localizar el botón Expand
-		cy.get(checkboxTreePage.getSelector.buttonExpandAll()).as('btnExpandAll');
+		cy.get(BUTTON_EXPAND_ALL).as('btnExpandAll');
 
 		checkboxTreePage.validateExpandTreePRC();
 
@@ -16,36 +20,41 @@ describe('US # GX3-5646 | ToolsQA | Elements | Checkbox', () => {
 		cy.get('@btnExpandAll').should('exist').click();
 
 		// Localizar los iconos de toggle de la rama
-		cy.get(checkboxTreePage.getSelector.imgToggleOpened()).as('imgToggleOpen');
+		cy.get(IMG_TOGGLE_OPENED).as('imgToggleOpen');
 		cy.get('@imgToggleOpen').should('exist'); //debe existir y estar abierta tras el click
 
 		// Localizar los icono de la ramas cerrados. No debiera haber ninguno
-		cy.get(checkboxTreePage.getSelector.imgToggleClosed()).should('not.exist');
+		cy.get(IMG_TOGGLE_CLOSED).should('not.exist');
 
 		// debe haber más de 1 (en principio son 17, pero puede cambiar)
 		cy.get('@chkNode').should('have.length.above', 1);
-	});
+	}); // it TC01
 
 	it('GX3-5661 | TC#02: Validar que el boton "Collapse All" cierre todas las ramas', () => {
+		const BUTTON_EXPAND_ALL = checkboxTreePage.getSelector.buttonExpandAll();
+		const BUTTON_COLLAPSE_ALL = checkboxTreePage.getSelector.buttonCollapseAll();
+		const IMG_TOGGLE_CLOSED = checkboxTreePage.getSelector.imgToggleClosed();
+
 		// PRC: Localizar el botón Expand All y hacer click
-		cy.get(checkboxTreePage.getSelector.buttonExpandAll()).as('btnExpandAll').click();
+		cy.get(BUTTON_EXPAND_ALL).as('btnExpandAll').click();
 
 		// Localizar el botón Collapse All
-		cy.get(checkboxTreePage.getSelector.buttonCollapseAll()).as('btnCollapseAll');
+		cy.get(BUTTON_COLLAPSE_ALL).as('btnCollapseAll');
 
 		checkboxTreePage.validateCollapseTreePRC();
 
 		// tras hacer click en Collapse
 		cy.get('@btnCollapseAll').should('exist').click();
-		cy.get(checkboxTreePage.getSelector.imgToggleClosed()).as('imgToggleClosed');
+		cy.get(IMG_TOGGLE_CLOSED).as('imgToggleClosed');
 
 		// debe haber sólo 1
 		cy.get('@chkNode').should('have.length', 1);
 		cy.get('@btnToggle').should('exist').and('have.length', 1);
-		cy.get('@imgToggleClosed').should('exist'); // y estar cerrada la rama
-	});
+		// y estar cerrada la rama
+		cy.get('@imgToggleClosed').should('exist');
+	}); // it TC02
 
-	it('GX3-5661 | TC#03: Validar comportamiento de expansión de toggles', () => {
+	it('GX3-5661 | TC#03: Validar que al expandir cada toggle el aspecto cambie adecuadamente', () => {
 		// comprobar que el árbol esta en el estado correcto antes de hacer nada
 		checkboxTreePage.validateToggleExpandiblePRC();
 
@@ -53,11 +62,13 @@ describe('US # GX3-5646 | ToolsQA | Elements | Checkbox', () => {
 
 		// Comprobar que el estado del árbol es correcto tras la expansión
 		checkboxTreePage.validateToggleExpandiblePSC();
-	});
+	}); // it TC03
 
-	it('GX3-5661 | TC#04: Validar comportamiento de colapso de toggles', () => {
+	it('GX3-5661 | TC#04: Validar que al colapsar cada toggle el aspecto cambie adecuadamente', () => {
+		const BUTTON_EXPAND_ALL = checkboxTreePage.getSelector.buttonExpandAll();
+
 		// PRC: Localizar el botón Expand All y hacer click
-		cy.get(checkboxTreePage.getSelector.buttonExpandAll()).as('btnExpandAll').click();
+		cy.get(BUTTON_EXPAND_ALL).as('btnExpandAll').click();
 
 		// Comprobar que el toggle esta en el estado correcto antes de empezar
 		checkboxTreePage.validateToggleCollapsablePRC();
@@ -67,37 +78,46 @@ describe('US # GX3-5646 | ToolsQA | Elements | Checkbox', () => {
 
 		// Comprobar estado del árbol tras la iteración
 		checkboxTreePage.validateToggleCollapsablePSC();
-	}); // it
+	}); // it TC04
 
-	it('GX3-5661 | TC#05: Validar comportamiento de checkboxes', () => {
+	it('GX3-5661 | TC#05: Validar que al marcar un checkbox contenedor se seleccionen los subelementos y se muestre el texto de la selección correctamente', () => {
+		const BUTTON_EXPAND_ALL = checkboxTreePage.getSelector.buttonExpandAll();
+		const LI_NODES = checkboxTreePage.getSelector.liNodes();
+		const CHECKBOX_TITLE = checkboxTreePage.getSelector.checkboxTitle();
+		const CHECKBOX_ELEMENT = checkboxTreePage.getSelector.checkboxElement();
+		const IMG_CHECKBOX_UNCHECKED_OR_HALF_CHECKED = checkboxTreePage.getSelector.imgCheckboxUncheckedOrHalfChecked();
+		const IMG_CHECKBOX_CHECKED_OR_HALF_CHECKED = checkboxTreePage.getSelector.imgCheckboxCheckedOrHalfChecked();
+		const TXT_RESULT = checkboxTreePage.getSelector.txtResult();
+		const TXT_SUCCESS = checkboxTreePage.getSelector.txtSuccess();
+
 		// PRC: Localizar el botón Expand All y hacer click
-		cy.get(checkboxTreePage.getSelector.buttonExpandAll()).as('btnExpandAll').click();
+		cy.get(BUTTON_EXPAND_ALL).as('btnExpandAll').click();
 
-		cy.get(checkboxTreePage.getSelector.liNodes()).then($checkboxItems => {
+		cy.get(LI_NODES).then($checkboxItems => {
 			// Obtiene un índice aleatorio
 			const randomIndex = Math.floor(Math.random() * ($checkboxItems.length - 1)) + 1;
 
 			// No se puede marcar directamente el input porque es invisible
 			// Selecciona el checkbox aleatorio y hace clic
 			cy.get($checkboxItems[randomIndex]).within($the => {
-				cy.get(checkboxTreePage.getSelector.checkboxTitle()).as('chkName');
+				cy.get(CHECKBOX_TITLE).as('chkName');
 
-				cy.get(checkboxTreePage.getSelector.checkboxElement()).as('chkListElement');
+				cy.get(CHECKBOX_ELEMENT).as('chkListElement');
 				cy.get('@chkListElement').first().click();
 
 				// Comprobar que los subelementos se han seleccionado
 				checkboxTreePage.validateSubNodesSelection('@chkListElement');
 
 				// No debe haber subelementos sin seleccionar o seleccionados a medias
-				cy.get(checkboxTreePage.getSelector.imgCheckboxUncheckedOrHalfChecked()).should('not.exist');
+				cy.get(IMG_CHECKBOX_UNCHECKED_OR_HALF_CHECKED).should('not.exist');
 
 				// El padre (li) del padre (ol) debe estar marcado o semimarcado
-				cy.get($the).parent().parent().first().as('liParent').find(checkboxTreePage.getSelector.imgCheckboxCheckedOrHalfChecked()).should('exist');
+				cy.get($the).parent().parent().first().as('liParent').find(IMG_CHECKBOX_CHECKED_OR_HALF_CHECKED).should('exist');
 			}); // $checkboxItems[randomIndex] within
 
 			// El Result debe ser visible
-			cy.get(checkboxTreePage.getSelector.txtResult()).should('exist').and('be.visible');
-			cy.get(checkboxTreePage.getSelector.txtSuccess()).should('exist').and('be.visible');
+			cy.get(TXT_RESULT).should('exist').and('be.visible');
+			cy.get(TXT_SUCCESS).should('exist').and('be.visible');
 		}); // li then
-	}); // it TC04
+	}); // it TC05
 });
--- cypress/support/pages/GX3-5646-Checkbox.Page.js
+++ cypress/support/pages/GX3-5646-Checkbox.Page.js
@@ -20,6 +20,7 @@ class CheckboxTree {
 		liNodesCollapsed: () => 'li.rct-node.rct-node-parent.rct-node-collapsed',
 		liNodesExpanded: () => 'li.rct-node.rct-node-parent.rct-node-expanded',
 		classNodeCollapsed: () => 'rct-node-collapsed',
+		classNodeExpanded: () => 'rct-node-expanded',
 
 		checkboxElement: () => 'span.rct-checkbox',
 		checkboxTitle: () => '.rct-title',
@@ -29,12 +30,16 @@ class CheckboxTree {
 	};
 
 	validateCollapseTreePRC() {
+		const BUTTON_COLLAPSE_TOGGLE = checkboxTreePage.getSelector.buttonCollapseToggle();
+		const IMG_TOGGLE_BUTTON = checkboxTreePage.getSelector.imgToggleButton();
+		const IMG_CHECKBOX = checkboxTreePage.getSelector.imgCheckbox();
+
 		// Localizar el botón de colapso de las ramas
-		cy.get(checkboxTreePage.getSelector.buttonCollapseToggle()).as('btnToggle');
+		cy.get(BUTTON_COLLAPSE_TOGGLE).as('btnToggle');
 		// Localizar el primer icono de colapso de las ramas [open | close]
-		cy.get(checkboxTreePage.getSelector.imgToggleButton()).eq(1).as('imgToggle');
+		cy.get(IMG_TOGGLE_BUTTON).eq(1).as('imgToggle');
 		// Localizar los checkbox ['check' | 'uncheck' | 'half-check']
-		cy.get(checkboxTreePage.getSelector.imgCheckbox()).as('chkNode');
+		cy.get(IMG_CHECKBOX).as('chkNode');
 
 		// Debe haber al menos 1
 		cy.get('@btnToggle').should('exist').and('have.length.above', 0);
@@ -43,12 +48,16 @@ class CheckboxTree {
 	}
 
 	validateExpandTreePRC() {
+		const BUTTON_COLLAPSE_TOGGLE = checkboxTreePage.getSelector.buttonCollapseToggle();
+		const IMG_TOGGLE_BUTTON = checkboxTreePage.getSelector.imgToggleButton();
+		const IMG_CHECKBOX = checkboxTreePage.getSelector.imgCheckbox();
+
 		// Localizar el botón de toggle de las ramas
-		cy.get(checkboxTreePage.getSelector.buttonCollapseToggle()).as('btnToggle');
+		cy.get(BUTTON_COLLAPSE_TOGGLE).as('btnToggle');
 		// Localizar la primera instancia de toggle de la rama (la posición 0 contiene All)
-		cy.get(checkboxTreePage.getSelector.imgToggleButton()).eq(1).as('imgToggle');
+		cy.get(IMG_TOGGLE_BUTTON).eq(1).as('imgToggle');
 		// Localizar los checkbox ['check' | 'uncheck' | 'half-check']
-		cy.get(checkboxTreePage.getSelector.imgCheckbox()).as('chkNode');
+		cy.get(IMG_CHECKBOX).as('chkNode');
 
 		// Debe haber al menos 1
 		cy.get('@btnToggle').should('exist').and('have.length.above', 0);
@@ -57,95 +66,105 @@ class CheckboxTree {
 	}
 
 	validateToggleCollapsablePRC() {
+		const LI_NODES_COLLAPSED = checkboxTreePage.getSelector.liNodesCollapsed();
+
 		// No debe haber nodos colapsados
-		cy.get(checkboxTreePage.getSelector.liNodesCollapsed()).should('not.exist');
+		cy.get(LI_NODES_COLLAPSED).should('not.exist');
 	}
 
 	validateToggleCollapsablePSC() {
+		const LI_NODES_EXPANDED = checkboxTreePage.getSelector.liNodesExpanded();
+
 		// Al acabar No debe haber nodos expandidos
-		cy.get(checkboxTreePage.getSelector.liNodesExpanded()).should('not.exist');
+		cy.get(LI_NODES_EXPANDED).should('not.exist');
 	}
 
 	validateToggleExpandiblePRC() {
+		const LI_NODES_EXPANDED = checkboxTreePage.getSelector.liNodesExpanded();
+
 		// No debe haber nodos expandidos
-		cy.get(checkboxTreePage.getSelector.liNodesExpanded()).should('not.exist');
+		cy.get(LI_NODES_EXPANDED).should('not.exist');
 	}
 
 	validateToggleExpandiblePSC() {
+		const LI_NODES_COLLAPSED = checkboxTreePage.getSelector.liNodesCollapsed();
+
 		// AL acabar NO debe haber nodos collapsados
-		cy.get(checkboxTreePage.getSelector.liNodesCollapsed()).should('not.exist');
-	}
-
-	validateToggleClick($the) {
-		cy.get($the).within(() => {
-			// Debe haber subelementos
-			cy.get('ol').should('exist');
-
-			// Debe tener boton Toggle
-			cy.get('button[title="Toggle"]').first().as('btnToggle').should('exist').and('be.visible').and('be.enabled').click();
-
-			// Tras pulsar el toggle
-			// NO debe haber subelementos
-			cy.get('ol').should('not.exist');
-		});
-
-		// La clase del Li ha cambiado a Collapsed
-		cy.get($the).should('have.class', 'rct-node-collapsed');
+		cy.get(LI_NODES_COLLAPSED).should('not.exist');
 	}
 
 	validateSubNodesSelection(pSelector) {
+		const CHECKBOX_ELEMENT = checkboxTreePage.getSelector.checkboxElement();
+		const IMG_CHECKBOX_CHECKED = checkboxTreePage.getSelector.imgCheckboxChecked();
+		const CHECKBOX_TITLE = checkboxTreePage.getSelector.checkboxTitle();
+
 		cy.get(pSelector).then($theList => {
 			// Obtener el recuento de elementos
 			const intCountNodes = Cypress.$($theList).length;
 
 			// Los checks estan marcados
-			cy.get(checkboxTreePage.getSelector.checkboxElement()).as('chkListElement');
-			cy.get(checkboxTreePage.getSelector.imgCheckboxChecked()).as('subElement').should('exist').and('have.length', intCountNodes);
+			cy.get(CHECKBOX_ELEMENT).as('chkListElement');
+			cy.get(IMG_CHECKBOX_CHECKED).as('subElement').should('exist').and('have.length', intCountNodes);
 
 			//Los elementos marcados deben estar en el result
 			cy.get('@subElement') //svg
 				.parent() // span rct-checkbox
 				.parent() // label tree-node-xxxx
-				.find(checkboxTreePage.getSelector.checkboxTitle()) // span rct-title
+				.find(CHECKBOX_TITLE) // span rct-title
 				.each($subThe => {
 					checkboxTreePage.validateTextResult($subThe);
 				}); // @subElement each
 		}); // @chkListElement then
 	}
 
-	validateTextResult($the) {
+	validateTextResult(pthe) {
+		const TREE_WRAPPER = checkboxTreePage.getSelector.treeWrapper();
+		const TXT_SUCCESS = checkboxTreePage.getSelector.txtSuccess();
+
 		// Convertir la cadena al mismo formato que muestra Result
 		// Aaaaa Bbbbb.doc -> aaaaaBbbbb (camelCase, sin extensión)
 
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-		const txtCadena = $the.text().toLowerCase().replace('.doc', '');
+		const txtCadena = pthe.text().toLowerCase().replace('.doc', '');
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 		const intPosEspacio = txtCadena.indexOf(' ');
 
-		cy.get($the).parentsUntil(checkboxTreePage.getSelector.treeWrapper()).parent().as('theFather');
+		cy.get(pthe).parentsUntil(TREE_WRAPPER).parent().as('theFather');
 
 		if (intPosEspacio > 0) {
 			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 			const txtNewCadena = txtCadena.slice(0, intPosEspacio) + txtCadena.charAt(intPosEspacio + 1).toUpperCase() + txtCadena.slice(intPosEspacio + 2);
 
-			cy.get('@theFather').find(checkboxTreePage.getSelector.txtSuccess()).as('txtResult').should('exist').and('include.text', txtNewCadena);
+			cy.get('@theFather').find(TXT_SUCCESS).as('txtResult').should('exist').and('include.text', txtNewCadena);
 		} else {
-			cy.get('@theFather').find(checkboxTreePage.getSelector.txtSuccess()).as('txtResult').should('exist').and('include.text', txtCadena);
+			cy.get('@theFather').find(TXT_SUCCESS).as('txtResult').should('exist').and('include.text', txtCadena);
 		} // if
 	}
 
 	expandNodes() {
 		const NODE_COLLAPSED = checkboxTreePage.getSelector.liNodesCollapsed();
+		const CLASS_NODE_EXPANDED = checkboxTreePage.getSelector.classNodeExpanded();
+		const CLASS_NODE_COLLAPSED = checkboxTreePage.getSelector.classNodeCollapsed();
+		const IMG_TOGGLE_CLOSED = checkboxTreePage.getSelector.imgToggleClosed();
+		const IMG_TOGGLE_OPENED = checkboxTreePage.getSelector.imgToggleOpened();
 
 		const $elements = Cypress.$(NODE_COLLAPSED);
 
 		if ($elements?.length > 0) {
 			cy.wrap($elements)
 				.each($the => {
-					// Debe tener boton Toggle
-					cy.get(checkboxTreePage.getSelector.buttonToggle()).first().as('btnToggle').should('exist').and('be.visible').and('be.enabled');
+					// Dado que no esta expandido no debe tener subelementos
+					cy.get($the).find('ol').should('not.exist');
+					cy.get($the).should('have.class', CLASS_NODE_COLLAPSED);
+					cy.get($the).find(IMG_TOGGLE_CLOSED).should('exist');
 
-					cy.wrap($the).find('button').first().click();
+					// Debe tener boton Toggle
+					cy.wrap($the).find('button').first().as('btnToggle').should('exist').and('be.visible').and('be.enabled').click();
+
+					// Tras expandirlo tiene que tener subelementos
+					cy.get($the).find('ol').should('exist');
+					cy.get($the).should('have.class', CLASS_NODE_EXPANDED);
+					cy.get($the).find(IMG_TOGGLE_OPENED).should('exist');
 				})
 				.then(() => {
 					checkboxTreePage.expandNodes();
@@ -156,24 +175,49 @@ class CheckboxTree {
 	}
 
 	collapseNodes() {
-		// Localizar la lista de Li
-		cy.get(checkboxTreePage.getSelector.liNodes()).as('expLiNodes');
+		const LI_NODES = checkboxTreePage.getSelector.liNodes();
 
+		// Localizar la lista de Li
 		// Debe haber al menos 1
-		cy.get('@expLiNodes').should('exist').and('be.visible').and('have.length.above', 0);
+		cy.get(LI_NODES).as('expLiNodes').should('exist').and('be.visible').and('have.length.above', 0);
 
 		cy.get('@expLiNodes').then($theList => {
-			// Obtener el recuento de elementos
-			const intCountNodes = Cypress.$($theList).length;
-
 			// Recorrer en orden inverso la lista de elementos
 			// para ir cerrandolos uno a uno.
 			// Si no la recorremos asi cerrariamos los nodos contenedores
 			const arrListRev = $theList.toArray().reverse();
 
-			arrListRev.forEach(($the, index) => {
-				checkboxTreePage.validateToggleClick($the);
+			arrListRev.forEach($the => {
+				checkboxTreePage.validateToggleCollapseClick($the);
 			});
+		});
+	}
+
+	validateToggleCollapseClick(pThe) {
+		const BUTTON_TOGGLE = checkboxTreePage.getSelector.buttonToggle();
+		const CLASS_NODE_EXPANDED = checkboxTreePage.getSelector.classNodeExpanded();
+		const CLASS_NODE_COLLAPSED = checkboxTreePage.getSelector.classNodeCollapsed();
+		const IMG_TOGGLE_CLOSED = checkboxTreePage.getSelector.imgToggleClosed();
+		const IMG_TOGGLE_OPENED = checkboxTreePage.getSelector.imgToggleOpened();
+
+		cy.get(pThe).within(() => {
+			// El nodo debe estar expandido
+			cy.root().should('have.class', CLASS_NODE_EXPANDED);
+			cy.get(IMG_TOGGLE_OPENED).should('exist');
+
+			// Debe haber subelementos
+			cy.get('ol').should('exist');
+
+			// Debe tener boton Toggle
+			cy.get(BUTTON_TOGGLE).first().as('btnToggle').should('exist').and('be.visible').and('be.enabled').click();
+
+			// Tras pulsar el toggle
+			// NO debe haber subelementos
+			cy.get('ol').should('not.exist');
+
+			// El nodo debe estar colapsado
+			cy.root().should('have.class', CLASS_NODE_COLLAPSED);
+			cy.get(IMG_TOGGLE_CLOSED).should('exist');
 		});
 	}
 }
