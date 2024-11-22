91c9193449ae18f838f59dfd5920bd9b4ff89e2b
Diego González Fernández <verogeid@gmail.com>
2024-11-22 11:55:32 +0100

    style (GX3-5590) Add constants to improve code reading

--- cypress/e2e/Tests/Elements/GX3-5590-Dynamic-Properties.cy.js
+++ cypress/e2e/Tests/Elements/GX3-5590-Dynamic-Properties.cy.js
@@ -5,21 +5,29 @@ describe('US GX3-5590 | ToolsQA | Elements | Dynamic Properties', () => {
 	});
 
 	it('US # GX3-5590 | TC#01: Validar el texto del tag paragraph', () => {
-		cy.get('[class="col-12 mt-4 col-md-6"] p').should('exist').and('contain.text', 'This text has random Id').and('be.visible');
+		const TXT_PARAGRAPH = '[class="col-12 mt-4 col-md-6"] p';
+
+		cy.get(TXT_PARAGRAPH).should('exist').and('contain.text', 'This text has random Id').and('be.visible');
 	});
 
 	it('US # GX3-5590 | TC#02: Validar el cambio de estado del button "enableAfter"', () => {
-		cy.get('[class="col-12 mt-4 col-md-6"] button#enableAfter.mt-4.btn.btn-primary').should('exist').and('be.not.enabled');
-		cy.get('[class="col-12 mt-4 col-md-6"] button#enableAfter', { timeout: 5000 }).should('exist').and('be.enabled');
+		const BUTTON_ENABLE_AFTER = '[class="col-12 mt-4 col-md-6"] button#enableAfter.mt-4.btn.btn-primary';
+
+		cy.get(BUTTON_ENABLE_AFTER).should('exist').and('be.not.enabled');
+		cy.get(BUTTON_ENABLE_AFTER, { timeout: 5000 }).should('exist').and('be.enabled');
 	});
 
 	it('US # GX3-5590 | TC#03: Validar el cambio de color del button "colorChange"', () => {
-		cy.get('[class="col-12 mt-4 col-md-6"] button#colorChange.mt-4.btn.btn-primary').should('exist').and('be.visible');
-		cy.get('[class="col-12 mt-4 col-md-6"] button#colorChange', { timeout: 5000 }).should('have.class', 'text-danger');
+		const BUTTON_COLOR_CHANGE = '[class="col-12 mt-4 col-md-6"] button#colorChange.mt-4.btn.btn-primary';
+
+		cy.get(BUTTON_COLOR_CHANGE).should('exist').and('be.visible');
+		cy.get(BUTTON_COLOR_CHANGE, { timeout: 5000 }).should('have.class', 'text-danger');
 	});
 
 	it('US # GX3-5590 | TC#04: Validar el cambio de visibilidad del button "visibleAfter"', () => {
-		cy.get('[class="col-12 mt-4 col-md-6"] button#visibleAfter.mt-4.btn.btn-primary').should('not.exist');
-		cy.get('[class="col-12 mt-4 col-md-6"] button#visibleAfter', { timeout: 5000 }).should('exist').and('be.visible');
+		const BUTTON_VISIBLE_AFTER = '[class="col-12 mt-4 col-md-6"] button#visibleAfter.mt-4.btn.btn-primary';
+
+		cy.get(BUTTON_VISIBLE_AFTER).should('not.exist');
+		cy.get(BUTTON_VISIBLE_AFTER, { timeout: 5000 }).should('exist').and('be.visible');
 	});
 });
